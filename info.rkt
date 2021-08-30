dc706ca3c58dabd46f428cc7d35360e35f13b447
Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
2021-08-30 14:57:24 -0700

    implement: info.rkt for racket 8

---
+++ info.rkt
@@ -0,0 +1,10 @@
+#lang info
+
+(define name "json-mapping")
+
+(define homepage "https://github.com/jeffhhk/racket-json-mapping")
+
+(define collection 'multi)
+
+(define deps '())
+(define version "0.2")
